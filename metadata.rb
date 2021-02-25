91c4125288e71ff38abb8a96655cc256a29dbaab
Stephen Higgins <stephen@gryphonandrook.com>
2021-02-25 05:51:50 -0700

    remove long description

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer 'Igor Rzegocki'
 maintainer_email 'igor@rzegocki.pl'
 license 'MIT'
 description 'Set of chef recipes for OpsWorks based Ruby projects'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+# long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.13.0'
 chef_version '~> 12.0' if respond_to?(:chef_version)
 
